bbdd2319d6933375ab07acb733bf0fb890cbb333
Cristian Cretu <crisemcr@gmail.com>
2025-03-10 20:02:39 +0200

    feat: test markdown

--- Duo/MainWindow.xaml.cs
+++ Duo/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System.Text;
+using Microsoft.Web.WebView2.Core;
+using System.ComponentModel;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -19,18 +22,110 @@ using Microsoft.UI.Xaml.Navigation;
 namespace Duo
 {
     /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private bool _previewVisible = false;
+        public bool PreviewVisible
+        {
+            get => _previewVisible;
+            set
+            {
+                if (_previewVisible != value)
+                {
+                    _previewVisible = value;
+                    OnPropertyChanged(nameof(PreviewVisible));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         public MainWindow()
         {
             this.InitializeComponent();
+            InitializeWebView();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private async void InitializeWebView()
         {
-            myButton.Content = "Clicked";
+            await MarkdownPreview.EnsureCoreWebView2Async();
+        }
+
+        private void TogglePreviewButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (PreviewVisible)
+            {
+                PreviewVisible = false;
+                TogglePreviewButton.Content = "Preview Markdown";
+            }
+            else
+            {
+                PreviewVisible = true;
+                TogglePreviewButton.Content = "Hide Preview";
+                RenderMarkdown();
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            MarkdownInput.Text = string.Empty;
+            if (PreviewVisible)
+            {
+                RenderMarkdown();
+            }
+        }
+
+        private void RenderMarkdown()
+        {
+            string markdownText = MarkdownInput.Text ?? string.Empty;
+            
+            string html = $@"
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <meta charset='utf-8'>
+                    <meta name='viewport' content='width=device-width, initial-scale=1'>
+                    <title>Markdown Preview</title>
+                    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/github-markdown-css/github-markdown.min.css'>
+                    <script src='https://cdn.jsdelivr.net/npm/marked/marked.min.js'></script>
+                    <style>
+                        .markdown-body {{
+                            box-sizing: border-box;
+                            min-width: 200px;
+                            max-width: 980px;
+                            margin: 0 auto;
+                            padding: 15px;
+                        }}
+                    </style>
+                </head>
+                <body>
+                    <div id='content' class='markdown-body'></div>
+                    <script>
+                        document.getElementById('content').innerHTML = marked.parse(`{EscapeJavaScriptString(markdownText)}`);
+                    </script>
+                </body>
+                </html>";
+
+            MarkdownPreview.NavigateToString(html);
+        }
+
+        private string EscapeJavaScriptString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return string.Empty;
+
+            return input
+                .Replace("\\", "\\\\")
+                .Replace("`", "\\`")
+                .Replace("\r", "\\r")
+                .Replace("\n", "\\n")
+                .Replace("\t", "\\t");
         }
     }
 }
